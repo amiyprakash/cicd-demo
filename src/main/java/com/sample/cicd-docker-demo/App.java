4ad5d33a106215fec6a6af6f3a0f1d4c98c62a65
amiy2991@gmail.com <amiy2992@gmail.com>
2020-10-14 14:01:13 +0530

    First Commit to the Repo

---
+++ src/main/java/com/sample/cicd-docker-demo/App.java
@@ -0,0 +1,13 @@
+package com.sample.cicd-docker-demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
